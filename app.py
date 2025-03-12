9a480cc117953ca78d2a70eca5a3403a25d04400
YuvalMadari <yuvalmadari1@gmail.com>
2025-03-12 11:36:25 +0200

    yuval GET /<room>

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-
-@app.route('/')
-def home():
-    return "Hello, world!"
-
+@app.route('/<room>')
+def home(room):
+    return render_template('index.html')
 
 
 if __name__== "__main__":
     app.run()
 
-print("print")
