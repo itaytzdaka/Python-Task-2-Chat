aef0bc9f1b3df52404d61f84d6f282b9e256cb53
itaytzdaka <itaytzdaka1@gmail.com>
2025-03-12 13:24:49 +0200

    itay: Implement GET /api/chat/<room>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,20 @@ def home():
 @app.route('/<room>')
 def room(room):
     return render_template('index.html')
+
+@app.route("/api/chat/<room>", methods=["GET", "POST"])
+def room_chat(room):
+
+    room_file_path = f"rooms/{room}.txt"
+
+    if request.method == "POST":
+        return "yuval implement"
+    else:
+        try:
+            with open(room_file_path, "r", encoding="utf-8") as file:
+                return file.read()
+        except FileNotFoundError:
+            return ""  # Return empty string if file does not exist
 
 
 if __name__== "__main__":
