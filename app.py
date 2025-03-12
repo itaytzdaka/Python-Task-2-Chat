08a7ceda8c9bdad879676e301768ed8db96c38b0
itaytzdaka <itaytzdaka1@gmail.com>
2025-03-12 11:07:28 +0200

    itay: return static index.html

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 
 @app.route('/')
+@app.route('/.')
 def home():
-    return "Hello, world!"
+    return render_template("index.html")
 
 
 
