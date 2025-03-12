d9032ddac2e33974577d6b2563ecd60a6ea07d05
YuvalMadari <yuvalmadari1@gmail.com>
2025-03-12 14:51:20 +0200

    Yuval : room_file_pat change

--- app.py
+++ app.py
@@ -26,15 +26,14 @@ def api(room):
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
-        file_path = f"rooms/{room}.txt"
 
         os.makedirs("rooms", exist_ok=True)
 
-        if not os.path.isfile(file_path):
-            with open(file_path, 'w', encoding='utf-8') as file:
+        if not os.path.isfile(room_file_path):
+            with open(room_file_path, 'w', encoding='utf-8') as file:
                 file.write(f"[{date}] {usr} : {msg}\n") 
         else:
-            with open(file_path, 'a', encoding='utf-8') as file:
+            with open(room_file_path, 'a', encoding='utf-8') as file:
                 file.write(f"[{date}] {usr} : {msg}\n") 
 
         return f"Message saved: [{date}] {usr} : {msg}"
