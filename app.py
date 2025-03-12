cc0f88e76a7e103cf5bcc9c1fa77c219397aed85
itaytzdaka <itaytzdaka1@gmail.com>
2025-03-12 12:39:54 +0200

    itay: fix home / route

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/.')
 @app.route('/')
-def home(room):
+def home():
     return render_template('index.html')
 
 @app.route('/<room>')
